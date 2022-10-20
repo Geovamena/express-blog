5c574cffc03c9f6f116364786481e422763924ed
SheykoWk <sheyko.wk@gmail.com>
2022-10-19 21:46:46 -0500

    add post

--- src/posts/posts.controller.js
+++ src/posts/posts.controller.js
@@ -1,9 +1,26 @@
 const Posts = require('../models/posts.models')
 
 const uuid = require('uuid')
+const Users = require('../models/users.models')
+const Categories = require('../models/categories.models')
 
 const getAllPosts = async() => {
-    const data = await Posts.findAll()
+    const data = await Posts.findAll({
+        include:[
+            {
+                model: Users
+            },
+            {
+                model: Categories,
+                attributes: {
+                    exclude: ['id']
+                }
+            }
+        ],
+        attributes: {
+            exclude: ['createdAt', 'updatedAt', 'categoryId']
+        }
+    })
     return data
 }
 
