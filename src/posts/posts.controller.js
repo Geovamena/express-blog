dedfeeedac4c08cb0108c23e7c806642075bae23
SheykoWk <sheyko.wk@gmail.com>
2022-10-19 21:25:14 -0500

    add posts feature

---
+++ src/posts/posts.controller.js
@@ -0,0 +1,30 @@
+const Posts = require('../models/posts.models')
+
+const uuid = require('uuid')
+
+const getAllPosts = async() => {
+    const data = await Posts.findAll()
+    return data
+}
+
+const getPostById = async(id) => {
+
+}
+
+const createPost = async (data) => {
+    const response = await Posts.create({
+        id: uuid.v4(),
+        title: data.title,
+        content: data.content,
+        createdBy: data.userId, //? este es el user id que viene desde el token
+        categoryId: data.categoryId
+    })
+    return response
+}
+
+module.exports = {
+    getAllPosts,
+    getPostById,
+    createPost
+}
+
